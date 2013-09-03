263e8582f18f82c488de4ac76a5bffecfd146a30
Rihan.Meij <Rihan.Meij@pamgolding.co.za>
2013-09-03 10:12:35 +0200

    Added a twitter app

---
+++ TwitterCount/TwitterCount/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TwitterCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.GetCounts = new System.Windows.Forms.Button();
+            this.Authorize = new System.Windows.Forms.Button();
+            this.Pin = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 51);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(503, 196);
+            this.Input.TabIndex = 0;
+            // 
+            // GetCounts
+            // 
+            this.GetCounts.Location = new System.Drawing.Point(215, 12);
+            this.GetCounts.Name = "GetCounts";
+            this.GetCounts.Size = new System.Drawing.Size(75, 23);
+            this.GetCounts.TabIndex = 1;
+            this.GetCounts.Text = "GetCounts";
+            this.GetCounts.UseVisualStyleBackColor = true;
+            this.GetCounts.Click += new System.EventHandler(this.GetCounts_Click);
+            // 
+            // Authorize
+            // 
+            this.Authorize.Location = new System.Drawing.Point(13, 13);
+            this.Authorize.Name = "Authorize";
+            this.Authorize.Size = new System.Drawing.Size(75, 23);
+            this.Authorize.TabIndex = 2;
+            this.Authorize.Text = "Authorize";
+            this.Authorize.UseVisualStyleBackColor = true;
+            this.Authorize.Click += new System.EventHandler(this.Authorize_Click);
+            // 
+            // Pin
+            // 
+            this.Pin.Location = new System.Drawing.Point(95, 15);
+            this.Pin.Name = "Pin";
+            this.Pin.Size = new System.Drawing.Size(100, 20);
+            this.Pin.TabIndex = 3;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(6, 293);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(508, 256);
+            this.Output.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 670);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Pin);
+            this.Controls.Add(this.Authorize);
+            this.Controls.Add(this.GetCounts);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button GetCounts;
+        private System.Windows.Forms.Button Authorize;
+        private System.Windows.Forms.TextBox Pin;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ TwitterCount/TwitterCount/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TweetSharp;
+
+
+namespace TwitterCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TwitterService twitterService;
+        OAuthRequestToken requestToken;
+
+        public void SetupTwitterClient()
+        {
+            TwitterClientInfo twitterClient = new TwitterClientInfo();
+            twitterClient.ConsumerKey = "4FTAZV9NXphr04Stq9yIpQ";
+            twitterClient.ConsumerSecret = "ZJ5yGtPrOcxsZQzsajnf8G8hDaP8QAwlRfEM0HUqHcc";
+            twitterService = new TwitterService(twitterClient);
+
+        }
+
+        private void GetCounts_Click(object sender, EventArgs e)
+        {
+            var accessToken = twitterService.GetAccessToken(requestToken, Pin.Text);
+            twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret);
+
+            List<string> outputStrings = new List<string>(Input.Lines.Count());
+
+            foreach (var line in Input.Lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    var profileInfo = twitterService.GetUserProfileFor(new GetUserProfileForOptions() { ScreenName = line });
+                    if (profileInfo != null)
+                    {
+                        outputStrings.Add(string.Format("{0} : {1}", line, profileInfo.FollowersCount));
+                    }
+                }
+            }
+
+            Output.Lines = outputStrings.ToArray();
+
+        }
+
+        private void Authorize_Click(object sender, EventArgs e)
+        {
+            SetupTwitterClient();
+          
+            requestToken = twitterService.GetRequestToken();
+            var authUrl = twitterService.GetAuthorizationUri(requestToken);
+
+            Process.Start(authUrl.ToString());
+        }
+    }
+
+    
+}
